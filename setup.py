7a1223dd33e8a22ac34b716f934689a086a5e5d8
ForzaElettromotrice <mingardi.federico@gmail.com>
2024-04-08 14:16:55 +0200

    Creata la prima versione del setup.py e aggiornato il .gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='QCSA',
+    packages=find_packages(include=['QCSA']),
+    version='0.1.0',
+    description='Boh, da scrivere',
+    author='ForzaElettromotrice',
+    python_requires=">=3.11",
+    install_requires=[],
+    extras_require={},
+    license_files=["LICENSE"]
+)
