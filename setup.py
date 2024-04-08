c9ae716e72877a46dc07e08fba664088fc6c578e
ForzaElettromotrice <mingardi.federico@gmail.com>
2024-04-08 15:37:52 +0200

    Aggiunta la mia mail alle info

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='QCSA',
-    packages=find_packages(include=['QCSA']),
-    version='0.1.0',
-    description='Boh, da scrivere',
-    author='ForzaElettromotrice',
-    python_requires=">=3.11",
-    install_requires=[],
-    extras_require={},
-    license_files=["LICENSE"]
+    name = 'QCSA',
+    packages = find_packages(include = ['QCSA']),
+    version = '0.1.0',
+    description = 'Boh, da scrivere',
+    author = 'ForzaElettromotrice',
+    author_email = 'mingardi.federico@gmail.com',
+    python_requires = '>=3.11',
+    install_requires = ['qiskit'],
+    extras_require = { },
+    license_files = ['LICENSE']
 )
